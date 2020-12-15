76c6da9be7dc04349ab85f36400651a7e087ee50
Soumya Ghosh Dastidar <gdsoumya@gmail.com>
2020-12-15 20:03:48 +0530

    Updated
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

--- server/server.go
+++ server/server.go
@@ -48,6 +48,7 @@ func main() {
 }
 
 func helloHandler(w http.ResponseWriter, req *http.Request) {
+	time.Sleep(100*time.Millisecond)
 	cxt := req.Context()
 	span := trace.SpanFromContext(cxt)
 	span.SetAttributes(semconv.HTTPRouteKey.String("hello"))
@@ -55,9 +56,9 @@ func helloHandler(w http.ResponseWriter, req *http.Request) {
 	var tracer = otel.Tracer("server")
 	cxt, span = tracer.Start(cxt, "server-span")
 	defer span.End()
-
+	time.Sleep(10*time.Millisecond)
 	projectID := baggage.Value(cxt, "ProjectID")
-	log.Print(projectID.AsString())
+	log.Print("Propagated Baggage: ", projectID.AsString())
 	span.SetAttributes(label.KeyValue{Key: "ProjectID", Value: projectID})
 	span.RecordError(errors.New("Error Test"))
 	span.SetStatus(codes.Ok,"normal error") // removes error status
